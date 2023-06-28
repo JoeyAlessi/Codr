f3c12a46c1615960db0b3feae2148cc74172a997
Doniyor <doniyori@buffalo.edu>
2023-06-28 10:04:25 -0400

    Added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ import EmptyLogo from './assets/Logo/Empty_Logo.png'
 
 const App = () => {
   return (
-    <div className="App">
-      <img src = {EmptyLogo} className = "empty-logo"></img>
-      <div className = "landing-page-buttons">
-      <div className = "type-writer-animation">
-      <h1 className="header">codr.</h1>
-      </div>
-      <h2 className="subheader">Connecting code and curiosity.</h2>
-      <img src = {Hand} className="handIMG"/>
-      <div className="get-started-button">
-        <button id = "button">
-          <text id = "get-started-text">Get Started</text>
-        </button>
-      </div>
-      </div>
-    </div>
+    // <div className="App">
+    //   <img src = {EmptyLogo} className = "empty-logo"></img>
+    //   <div className = "landing-page-buttons">
+    //   <div className = "type-writer-animation">
+    //   <h1 className="header">codr.</h1>
+    //   </div>
+    //   <h2 className="subheader">Connecting code and curiosity.</h2>
+    //   <img src = {Hand} className="handIMG"/>
+    //   <div className="get-started-button">
+    //     <button id = "button">
+    //       <text id = "get-started-text">Get Started</text>
+    //     </button>
+    //   </div>
+    //   </div>
+    // </div>
+    
+
   );
 };
 
