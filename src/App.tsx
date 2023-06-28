304aa54f9f089831e43c4663cf21e42a60d5a6fa
joechen43 <jchen526@buffalo.edu>
2023-06-28 09:42:47 -0400

    Added animations, logo, and fixed gradient

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import Hand from "./assets/ai-nuclear-energy-background-future-innovation-disruptive-technology-removebg.png"
+import EmptyLogo from './assets/Logo/Empty_Logo.png'
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Codr</h1>
+      <img src = {EmptyLogo} className = "empty-logo"></img>
+      <div className = "landing-page-buttons">
+      <div className = "type-writer-animation">
+      <h1 className="header">codr.</h1>
+      </div>
+      <h2 className="subheader">Connecting code and curiosity.</h2>
+      <img src = {Hand} className="handIMG"/>
+      <div className="get-started-button">
+        <button id = "button">
+          <text id = "get-started-text">Get Started</text>
+        </button>
+      </div>
+      </div>
     </div>
   );
 };
