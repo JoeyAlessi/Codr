09e3be4919da300c4b82cef47d95f4a03d27fde7
Doniyor <doniyori@buffalo.edu>
2023-06-27 22:59:41 -0400

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-
+import Hand from "./assets/ai-nuclear-energy-background-future-innovation-disruptive-technology-removebg.png"
 const App = () => {
   return (
     <div className="App">
-      <h1>Codr</h1>
+      <h1 className="text">codr.</h1>
+      <img src = {Hand} className="handIMG"/>
     </div>
   );
 };
