d16538c4acc5d128f5a196e64b0ff66d347d72a9
Doniyor <doniyori@buffalo.edu>
2023-06-28 10:43:53 -0400

    IDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-import Hand from "./assets/ai-nuclear-energy-background-future-innovation-disruptive-technology-removebg.png"
-import EmptyLogo from './assets/Logo/Empty_Logo.png'
-
+import Hand from "./assets/ai-nuclear-energy-background-future-innovation-disruptive-technology-removebg.png";
+import EmptyLogo from "./assets/Logo/Empty_Logo.png";
+import Home from "./components/Home"
 
 const App = () => {
   return (
-    // <div className="App">
-    //   <img src = {EmptyLogo} className = "empty-logo"></img>
-    //   <div className = "landing-page-buttons">
-    //   <div className = "type-writer-animation">
-    //   <h1 className="header">codr.</h1>
-    //   </div>
-    //   <h2 className="subheader">Connecting code and curiosity.</h2>
-    //   <img src = {Hand} className="handIMG"/>
-    //   <div className="get-started-button">
-    //     <button id = "button">
-    //       <text id = "get-started-text">Get Started</text>
-    //     </button>
-    //   </div>
-    //   </div>
-    // </div>
-    
-
+    <>
+    <Home/>
+    </>
   );
 };
-
 
 export default App;
